bfe1df9ce5eb89dc23f3e931c24e721a7a1d2ced
D-eject <vitinhavitor@hotmail.com>
2016-11-26 16:08:31 +0000

    Remoção de algumas coisas

--- Runners2016V.java
+++ Runners2016V.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package runners2016v;
 
 import java.util.Scanner;
 
-/**
- *
- * @author Vitor Melo
- */
 public class Runners2016V {
 
     private static final int NUM_SOCIOS = 100;
