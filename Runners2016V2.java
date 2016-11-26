c028bdca4f413881222f3209b365b82bb9fed41a
D-eject <vitinhavitor@hotmail.com>
2016-11-26 16:31:05 +0000

    Mudanças Menu
    Falta \n na linha 30

--- Runners2016V2.java
+++ Runners2016V2.java
@@ -3,7 +3,7 @@ package runners2016v2;
 import java.util.Scanner;
 
 public class Runners2016V2 {
-
+    
     private static final int NUM_SOCIOS = 100;
     private static final int NUM_PROVAS = 5;
     /**
@@ -21,6 +21,17 @@ public class Runners2016V2 {
     static Scanner scan = new Scanner(System.in);
 
     public static void menu(){
+        System.out.printf("1 - Ler um ficheiro de socios\n"
+                + "2 - Mostrar informacao sobre atletas\n"
+                + "3 - Modificar um campo de um atleta\n"
+                + "4 - Ler inscricoes\n"
+                + "5 - Ler ficheiro de uma prova\n"
+                + "6 - Mostrar / guardar todas as informacoes\n"
+                + "7 - Limpar todos os dados"
+                + "8 - Mostrar atletas melhores/piores que a média de uma prova\n"
+                + "9 - Mostrar percentagem de mulheres e desistencias de uma prova\n"
+                + "10 - Criar ficheiro formatado com nome - idade - media de tempo\n"
+                + "11 - Mostrar vencedor de uma prova\n");//Supostamente este sai para o teste
         switch(scan.nextInt()){
             case 1:
                 //TODO ler nome do ficheiro dos atletas
